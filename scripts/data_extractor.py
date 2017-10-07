d853e133c46b61263a9cd5d0f032fbc5101e6041
sabSAThai <pranavsankhe40@gmail.com>
2017-10-07 14:07:56 +0530

    script to store mat files

--- scripts/data_extractor.py
+++ scripts/data_extractor.py
@@ -3,7 +3,10 @@ import scipy.io
 
 if __name__ == '__main__':
 	# after saving the data in .mat files extract in python files
-    mat_raw_eeg = scipy.io.loadmat('raw_eeg.mat')          # 69 x 2478166
-    mat_events_eeg = scipy.io.loadmat('events_eeg.mat')    # 540 x 3
+    mat_raw_eeg = scipy.io.loadmat('/home/tinkerers/pranav_sankhe/Perceived-Music-Information-Retrieval/MIIR_dataset/mat_files/eeg/P01-raw.mat')          # 69 x 2478166
+    mat_events_eeg = scipy.io.loadmat('/home/tinkerers/pranav_sankhe/Perceived-Music-Information-Retrieval/MIIR_dataset/mat_files/events/P01-raw.mat')    # 540 x 3
+    
     print np.shape(mat_raw_eeg['data'])
     print np.shape(mat_events_eeg['data'])
+
+    print len(mat_raw_eeg['data'])
--- scripts/fif2mat.py
+++ scripts/fif2mat.py
@@ -1,10 +1,4 @@
 #!/usr/bin/env python
-__author__ = 'sstober'
-'''
-Created on Jun 16, 2015
-
-@author: sstober
-'''
 
 import argparse
 import mne
@@ -13,37 +7,46 @@ from scipy import io
 
 if __name__ == '__main__':
     
-    parser = argparse.ArgumentParser(prog='fif2mat',
-                                     description='converts raw EEG data in FIF format into MAT format;'
-                                                 'events are stored in an extra file in EEGLab format.')
+    # parser = argparse.ArgumentParser(prog='fif2mat',
+    #                                  description='converts raw EEG data in FIF format into MAT format;'
+    #                                              'events are stored in an extra file in EEGLab format.')
 
-    parser.add_argument('fif_filepath',
-                        help='path to the input FIF file')
+    # parser.add_argument('fif_filepath',
+    #                     help='path to the input FIF file')
 
-    parser.add_argument('eeg_filepath',
-                        help='path to the output MAT file for the raw EEG data')
+    # parser.add_argument('eeg_filepath',
+    #                     help='path to the output MAT file for the raw EEG data')
 
-    parser.add_argument('events_filepath',
-                        help='path to the output MAT file for the events')
+    # parser.add_argument('events_filepath',
+    #                     help='path to the output MAT file for the events')
 
-    args = parser.parse_args()
 
-    print 'loading raw data from ', args.fif_filepath
-    raw = mne.io.Raw(args.fif_filepath, preload=True, verbose=True)
-    data, time = raw[:,:]
+    # args = parser.parse_args()
 
-    print 'saving raw data to', args.eeg_filepath
-    io.savemat(args.eeg_filepath, dict(data=data), oned_as='row')
+    file_list = ['P01-raw', 'P04-raw', 'P05-raw', 'P06-raw', 'P07-raw', 'P09-raw', 'P11-raw', 'P12-raw', 'P13-raw', 'P14-raw' ]
+  
+    for file in file_list:
 
-    events = mne.find_events(raw, stim_channel='STI 014', shortest_event=0)
+        fif_filepath = '../MIIR_dataset/fif_files/' + file + '.fif'
+        eeg_filepath = '../MIIR_dataset/mat_files/eeg/' + file
+        events_filepath = '../MIIR_dataset/mat_files/events/' + file
 
-    # EEGLab event structure: type, latency, urevent
-    # Event latencies are stored in units of data sample points relative to (0) 
-    # the beginning of the continuous data matrix (EEG.data).
-    eeglab_events = [[event[2], event[0], 0] for event in events]
-    eeglab_events = np.asarray(eeglab_events, dtype=int)
-    
-    print 'saving events to', args.events_filepath
-    io.savemat(args.events_filepath, dict(data=eeglab_events), oned_as='row')
+        print 'loading raw data from ', fif_filepath
+        raw = mne.io.Raw(fif_filepath, preload=True, verbose=True)
+        data, time = raw[:,:]
+
+        print 'saving raw data to', eeg_filepath
+        io.savemat(eeg_filepath, dict(data=data), oned_as='row')
+
+        events = mne.find_events(raw, stim_channel='STI 014', shortest_event=0)
+
+        # EEGLab event structure: type, latency, urevent
+        # Event latencies are stored in units of data sample points relative to (0) 
+        # the beginning of the continuous data matrix (EEG.data).
+        eeglab_events = [[event[2], event[0], 0] for event in events]
+        eeglab_events = np.asarray(eeglab_events, dtype=int)
+
+        print 'saving events to', events_filepath
+        io.savemat(events_filepath, dict(data=eeglab_events), oned_as='row')
 
     
